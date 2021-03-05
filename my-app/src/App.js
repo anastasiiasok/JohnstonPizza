636fd73a593be8747ea3282e568ef5402f8923cf
Anastasiia Soktoeva <anastasiiasoktoeva@anastasiias-air.myfiosgateway.com>
2021-03-04 20:47:05 -0500

    set active item in the sort button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,6 @@ function App() {
                 items={[
                   "Meat",
                   "Vegetarian",
-                  "Grilled",
                   "Spicy",
                   "Salads",
                   "Cheesesticks",
